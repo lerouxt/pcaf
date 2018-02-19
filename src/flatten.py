422390044ba87726f41eed58c5f89c95000ee3ea
Tom LeRoux <tom@harborcoat.com>
2018-02-19 23:25:14 +0000

    fix the followup flag columns

--- src/flatten.py
+++ src/flatten.py
@@ -11,7 +11,7 @@ def flatten(infile, outfile):
 
     df = pandas.read_csv(infile)
 
-    right_cols = [ 'case_id', 'phq9_9', 'phq9_total', 'psychoeducation_flag', 'functioning_total', 
+    right_cols = [ 'case_id', 'followup_visit', 'preipt_visit', 'phq9_9', 'phq9_total', 'psychoeducation_flag', 'functioning_total', 
                    'depressed_flag', 'severely_depressed_flag', 'suicidal_flag' ]
 
     df_initial = df[df.initial_visit == 1]
@@ -24,6 +24,9 @@ def flatten(infile, outfile):
 
     df_final = df_initial.join(df_followup.set_index('case_id'), on='case_id', rsuffix='_ifu')
     df_final = df_final.join(df_ipt.set_index('case_id'), on='case_id', rsuffix='_ipt')
+    df_final['followup_visit'] = df_final['followup_visit_ifu']
+    df_final['preipt_visit'] = df_final['preipt_visit_ipt']
+    df_final = df_final.drop(columns=['followup_visit_ifu', 'followup_visit_ipt', 'preipt_visit_ifu', 'preipt_visit_ipt'])
 
     #wtr = pandas.ExcelWriter(outfile)
     df_final.to_csv(outfile, index=False)
